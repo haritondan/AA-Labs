c76a01a3d04f7106bb6e19670b682c583bf54a11
frateleitachi <danyelhr22@gmail.com>
2023-05-12 14:59:00 +0300

    some changes on lab5

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,147 +1,106 @@
-import math
-import random
 import numpy as np
-from matplotlib import pyplot as plt
-from graph import generate_sparse_graph, generate_complete_graph, draw_graph, floyd, dijkstra
 import time
+import networkx as nx
+import matplotlib.pyplot as plt
 
+# Dijkstra's algorithm implementation
+def dijkstra(graph, start):
+    n = len(graph)
+    dist = [float('inf')] * n
+    visited = [False] * n
+    dist[start] = 0
+    for _ in range(n):
+        u = min(filter(lambda x: not visited[x], range(n)), key=dist.__getitem__)
+        visited[u] = True
+        for v in range(n):
+            if graph[u][v] != 0 and not visited[v]:
+                alt = dist[u] + graph[u][v]
+                if alt < dist[v]:
+                    dist[v] = alt
+    return dist
 
-dense_graph = generate_complete_graph(10)
-draw_graph(dense_graph, 'Dense Graph')
+# Floyd-Warshall algorithm implementation
+def floyd_warshall(graph):
+    n = len(graph)
+    dist = graph.copy()
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+    return dist
 
-sparse_graph = generate_sparse_graph(10, 0.3)
-draw_graph(sparse_graph, 'Sparse Graph')
+# Function to draw graph using NetworkX
+def draw_graph(graph):
+    G = nx.DiGraph()
+    n = len(graph)
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] != 0:
+                G.add_edge(i, j, weight=graph[i][j])
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True)
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    plt.show()
 
-random.seed(100)
+# Measure execution time of Dijkstra's algorithm for increasing number of nodes in sparse graph
+times_sparse_dijkstra = []
+for n in range(10, 101, 10):
+    graph_sparse = np.random.choice([0, 1], size=(n, n), p=[0.9, 0.1])
+    start_time = time.time()
+    dijkstra(graph_sparse, 0)
+    end_time = time.time()
+    times_sparse_dijkstra.append(end_time - start_time)
 
-times_floyd = list()
-times_dj = list()
-nodes = [5, 25, 50, 100]
-nodes_searched = list()
-node_search = list()
-digit_num = 6
-image_num = 0
+# Measure execution time of Floyd-Warshall algorithm for increasing number of nodes in sparse graph
+times_sparse_floyd = []
+for n in range(10, 101, 10):
+    graph_sparse = np.random.choice([0, 1], size=(n, n), p=[0.9, 0.1])
+    start_time = time.time()
+    floyd_warshall(graph_sparse)
+    end_time = time.time()
+    times_sparse_floyd.append(end_time - start_time)
 
-for i in nodes:
+# Measure execution time of Dijkstra's algorithm for increasing number of nodes in dense graph
+times_dense_dijkstra = []
+for n in range(10, 101, 10):
+    graph_dense = np.random.randint(0, 10, size=(n, n))
+    start_time = time.time()
+    dijkstra(graph_dense, 0)
+    end_time = time.time()
+    times_dense_dijkstra.append(end_time - start_time)
 
-    current_times_dj = list()
-    current_times_floyd = list()
-    random_nodes = list()
-    nodes_searched.append('Graph ' + str(i))
-    node_search.append('Graph: ' + str(i))
+# Measure execution time of Floyd-Warshall algorithm for increasing number of nodes in dense graph
+times_dense_floyd = []
+for n in range(10, 101, 10):
+    graph_dense = np.random.randint(0, 10, size=(n, n))
+    start_time = time.time()
+    floyd_warshall(graph_dense)
+    end_time = time.time()
+    times_dense_floyd.append(end_time - start_time)
 
-    # Sparse graph
-    nodes_searched.append('Sparse: ')
-    node_search.append('Sparse: ')
-    graph_sparse = generate_sparse_graph(i, 2 / i)
+#
+# Draw sample sparse graph
+graph_sparse = np.random.choice([0, 1], size=(10, 10), p=[0.9, 0.1])
+draw_graph(graph_sparse)
 
-    if i / 5 >= 5:
-        search = 5
-    else:
-        search = math.floor(i / 5)
-
-    for j in range(search):
-        start_node = str(random.randint((j + 1), (j + 1) * 2))
-        end_node = str(random.randint((j + 1) * 3, (j + 1) * 5))
-
-        # Dijkstra
-        start_time = time.perf_counter()
-        shortest_path_dj = dijkstra(graph_sparse, start_node, end_node)
-        end_time = time.perf_counter()
-        time_taken = end_time - start_time
-        current_times_dj.append(round(time_taken, digit_num))
-
-        # Floyd
-
-        start_time = time.perf_counter()
-        shortest_path_floyd = floyd(graph_sparse, start_node, end_node)
-        end_time = time.perf_counter()
-        time_taken = end_time - start_time
-        current_times_floyd.append(round(time_taken, digit_num))
-
-        random_nodes.append([start_node, end_node])
-        nodes_searched.append(start_node + '-' + end_node + ' = ' + str(shortest_path_dj) + ' | ')
-        node_search.append(start_node + '-' + end_node + ' | ')
-
-    # Dense graph
-    nodes_searched.append('Dense: ')
-    node_search.append('Dense: ')
-    graph_dense = generate_complete_graph(i)
-
-    for j in random_nodes:
-        start_node = j[0]
-        end_node = j[1]
-
-        # Dijkstra
-
-        start_time = time.perf_counter()
-        shortest_path_dj = dijkstra(graph_dense, start_node, end_node)
-        end_time = time.perf_counter()
-        time_taken = end_time - start_time
-        current_times_dj.append(round(time_taken, digit_num))
-
-        # Floyd
-
-        start_time = time.perf_counter()
-        shortest_path_floyd = floyd(graph_dense, start_node, end_node)
-        end_time = time.perf_counter()
-        time_taken = end_time - start_time
-        current_times_floyd.append(round(time_taken, digit_num))
-
-        nodes_searched.append(start_node + '-' + end_node + ' = ' + str(shortest_path_dj) + ' | ')
-        node_search.append(start_node + '-' + end_node + ' | ')
-
-    times_floyd.append(current_times_floyd)
-    times_dj.append(current_times_dj)
-
-
-arr = [i for i in range(5)]
-x = np.arange(1, len(arr)+1)
-
-# Dijkstra
-plt.figure(image_num)
-plt.bar(x-0.2, times_dj[2][0:5], 0.2, label='Sparse 50', color='blue')
-plt.bar(x-0.1, times_dj[3][0:5], 0.2, label='Sparse 100', color='grey')
-plt.bar(x+0.2, times_dj[2][5:10], 0.2, label='Dense 50', color='purple')
-plt.bar(x+0.3, times_dj[3][5:10], 0.2, label='Dense 100', color='pink')
-plt.xlabel('Check')
-plt.ylabel('Time(s)')
-plt.title('Dijkstra:')
+# Draw sample dense graph
+graph_dense = np.random.randint(0, 10, size=(10, 10))
+draw_graph(graph_dense)
+# Plot execution time data for sparse graph
+plt.plot(range(10, 101, 10), times_sparse_dijkstra, label="Dijkstra's Algorithm (Sparse)")
+plt.plot(range(10, 101, 10), times_sparse_floyd, label="Floyd-Warshall Algorithm (Sparse)")
 plt.legend()
+plt.xlabel("Number of Nodes")
+plt.ylabel("Execution Time (Seconds)")
+plt.title("Execution Time of Shortest Path Algorithms on Sparse Graph")
+plt.show()
 
-# Update x-ticks labels
-custom_labels = ['Node 1', 'Node 2', 'Node 3', 'Node 4', 'Node 5']
-plt.xticks(x, custom_labels)
-
-image_num += 1
-
-plt.figure(image_num)
-plt.bar(x-0.2, times_floyd[2][0:5], 0.2, label='Sparse 50', color='blue')
-plt.bar(x-0.1, times_floyd[3][0:5], 0.2, label='Sparse 100', color='red')
-plt.bar(x+0.2, times_floyd[2][5:10], 0.2, label='Dense 50', color='black')
-plt.bar(x+0.3, times_floyd[3][5:10], 0.2, label='Dense 100', color='orange')
-plt.xlabel('Check')
-plt.ylabel('Time(s)')
-plt.title('Floyd:')
+# Plot execution time data for dense graph
+plt.plot(range(10, 101, 10), times_dense_dijkstra, label="Dijkstra's Algorithm (Dense)")
+plt.plot(range(10, 101, 10), times_dense_floyd, label="Floyd-Warshall Algorithm (Dense)")
 plt.legend()
-
-custom_labels = ['Node 1', 'Node 2', 'Node 3', 'Node 4', 'Node 5']
-plt.xticks(x, custom_labels)
-
-image_num += 1
-
-plt.figure(image_num)
-plt.bar(x-0.3, times_dj[3][0:5], 0.2, label='Dijkstra sparse 100', color='green')
-plt.bar(x+0.1, times_floyd[3][0:5], 0.2, label='Floyd sparse 100', color='magenta')
-plt.bar(x+0.2, times_dj[3][5:10], 0.2, label='Dijkstra dense 100', color='orange')
-plt.bar(x+0.4, times_floyd[3][5:10], 0.2, label='Floyd dense 100', color='purple')
-plt.xlabel('Check')
-plt.ylabel('Time(s)')
-plt.title('Dijkstra vs Floyd')
-plt.legend()
-
-custom_labels = ['Node 1', 'Node 2', 'Node 3', 'Node 4', 'Node 5']
-plt.xticks(x, custom_labels)
-image_num += 1
-
+plt.xlabel("Number of Nodes")
+plt.ylabel("Execution Time (Seconds)")
+plt.title("Execution Time of Shortest Path Algorithms on Dense Graph")
 plt.show()
